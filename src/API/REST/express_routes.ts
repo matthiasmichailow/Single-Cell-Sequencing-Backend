b46313aa2f170f5852756f5188d8580a4cecd807
Yi Rui Cui <yirui.cui@tum.de>
2021-10-12 17:12:37 +0200

    added call to updateProfile route in express_routes.ts

--- src/API/REST/express_routes.ts
+++ src/API/REST/express_routes.ts
@@ -1,13 +1,15 @@
 import REST_Host from "./_rest_host";
 import express, {Router} from "express";
 import register_route from "./routes/register";
+import update_profile_route from "./routes/updateProfile";
 
 // setup the websocket-server on top of the http_server
-export function express_routes(this:REST_Host) : Router {
+export function express_routes(this: REST_Host): Router {
     let router = express.Router();
 
     // TODO handle our REST routes
     this.expressApp.use(register_route());
+    this.expressApp.use(update_profile_route());
     this.expressApp.use((req, res) => res.status(404).send("Not found."));
 
     return router;
